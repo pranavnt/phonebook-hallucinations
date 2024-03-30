f5900c69510f15ef1537ee8e0816262140377af0
Pranav Teegavarapu <pranavnt@outlook.com>
2024-03-30 01:31:11 -0700

    fake vAliDaTiOn

---
+++ phonebook_hallucinations/model.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sentencepiece import SentencePieceProcessor
+from pathlib import Path
+from typing import List
+import math
+
+class Tokenizer:
+    def __init__(self, model_path: str):
+        assert Path(model_path).exists(), model_path
+        self._model = SentencePieceProcessor(model_file=model_path)
+        assert self._model.vocab_size() == self._model.get_piece_size()
+
+    @property
+    def eos_id(self) -> int:
+        return self._model.eos_id()
+
+    @property
+    def pad_id(self) -> int:
+        return self._model.pad_id()
+
+    def encode(self, s: str) -> List[int]:
+        return [self._model.bos_id(), *self._model.encode(s)]
+
+    def batch_encode(self, batch: List[str]) -> List[List[int]]:
+        data = torch.zeros(len(batch), 512, dtype=torch.int64)
+        for i, s in enumerate(batch):
+            encoded = torch.tensor(self._model.encode(s))
+            data[i, :len(encoded)] = encoded
+        return data
+
+    def decode(self, t: List[int]) -> str:
+        return self._model.decode(t)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = nn.MultiheadAttention(embed_dim=embed_size, num_heads=heads, dropout=dropout)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size)
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(query, key, value, attn_mask=mask)[0]
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000, device=None):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+
+        if device is not None:
+            self.dropout.to(device)
+            self.pe = pe.to(device)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
+
+class Transformer(nn.Module):
+    def __init__(self, vocab_size, embed_size, num_layers, heads, dropout, forward_expansion, device=None):
+        super(Transformer, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.pos_embedding = PositionalEncoding(embed_size, dropout, device=device)
+        self.transformer_layers = nn.ModuleList([TransformerBlock(embed_size, heads, dropout, forward_expansion) for _ in range(num_layers)])
+
+    def forward(self, x, mask=None):
+        out = x
+        out = self.pos_embedding(out)
+        for layer in self.transformer_layers:
+            out = layer(out, out, out, mask)
+        return out
---
+++ phonebook_hallucinations/train.py
@@ -0,0 +1,80 @@
+from model import Tokenizer, Transformer
+from data import FakePhoneDataset
+from torch.utils.data import DataLoader
+import pickle
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import Adam
+import os
+import wandb
+
+dataset = pickle.load(open("phonebook.pkl", "rb"))
+dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
+val = FakePhoneDataset(num_people=50, num_samples=32)
+val_dataloader = DataLoader(val, batch_size=16, shuffle=True)
+
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
+tokenizer = Tokenizer("./tokenizer.model")
+model = Transformer(
+    embed_size=512,
+    vocab_size=32000,
+    num_layers=6,
+    heads=8,
+    dropout=0.1,
+    forward_expansion=1024,
+    device=device
+)
+
+model.to(device)
+
+optimizer = Adam(model.parameters(), lr=0.001)
+
+num_epochs = 10
+
+wandb.init(project="phonebook_hallucinations", entity="pranavnt", config={
+    "learning_rate": 0.001,
+    "batch_size": 16,
+    "num_epochs": 10
+})
+
+if not os.path.exists("./checkpoint"):
+    os.makedirs("./checkpoint")
+
+for epoch in range(num_epochs):
+    for batch_idx, batch in enumerate(dataloader):
+        batch = tokenizer.batch_encode(batch).to(device)
+
+        out = model(batch)
+
+        loss = F.cross_entropy(out, batch)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 50 == 0:
+            print(f"Epoch: {epoch}, Batch: {batch_idx}, Loss: {loss.item()}")
+            wandb.log({
+                "epoch": epoch,
+                "batch": batch_idx,
+                "loss": loss.item()
+            })
+
+        if batch_idx % 250 == 0:
+            torch.save(model.state_dict(), f"./checkpoint/phonebook_transformer_{epoch}_{batch_idx}.pt")
+
+            for batch_val in val_dataloader:
+                batch_val = tokenizer.batch_encode(batch_val).to(device)
+                out = model(batch_val)
+                loss = F.cross_entropy(out, batch_val)
+                wandb.log({
+                    "epoch": epoch,
+                    "batch": batch_idx,
+                    "val_loss": loss.item()
+                })
+    
+    torch.save(model.state_dict(), f"./checkpoint/phonebook_transformer_{epoch}.pt")
+    wandb.save(f"./checkpoint/phonebook_transformer_{epoch}.pt")
+
